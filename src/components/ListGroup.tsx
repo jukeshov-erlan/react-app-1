27d272c78bcf15336669677281b4f74f89857b0a
jukeshov erlan <jukeshov.erlan@gmail.com>
2025-03-03 21:00:04 +0600

    second update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,28 @@
-function ListGroup() {
-  const items = ["New York", "San Francisco", "London", "Paris", "Berlin"];
+import { useState } from "react";
 
+function ListGroup() {
+  let items = ["New York", "San Francisco", "London", "Paris", "Berlin"];
+  let selectedIndex = 0;
+  //hook
+  useState
+  // Event handler Обработчик события
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No item found </p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={index}
+            onClick={() => {selectedIndex = index;}}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </>
