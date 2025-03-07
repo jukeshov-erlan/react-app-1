c30d2b020d414cfa1071393fa324d16f00cee39b
jukeshov erlan <jukeshov.erlan@gmail.com>
2025-03-08 02:03:15 +0600

    added ts.config.json for correct import/export modules

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div><ListGroup/></div>
+  return (
+    <div>
+      <ListGroup />
+    </div>
+  );
 }
 
 export default App;
